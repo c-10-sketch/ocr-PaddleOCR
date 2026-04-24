021d752f3d6060c01c50f89d692a2136e8cdb846
MrTusarRX <mrtusarrx@gmail.com>
2026-04-24 17:35:09 +0530

    paddleocr api

---
+++ README.md
@@ -0,0 +1,14 @@
+---
+title: Ocr Api
+emoji: 📊
+colorFrom: indigo
+colorTo: indigo
+sdk: gradio
+sdk_version: 6.13.0
+app_file: app.py
+pinned: false
+license: mit
+short_description: ocr-api
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, UploadFile, File
+from paddleocr import PaddleOCR
+import numpy as np
+import cv2
+
+app = FastAPI()
+
+# Load model once
+ocr = PaddleOCR(use_angle_cls=True, lang='en')
+
+@app.post("/ocr")
+async def ocr_api(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+
+    np_arr = np.frombuffer(image_bytes, np.uint8)
+    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+    result = ocr.ocr(img, cls=True)
+
+    text = []
+    for line in result:
+        for word in line:
+            text.append(word[1][0])
+
+    return {"text": " ".join(text)}
